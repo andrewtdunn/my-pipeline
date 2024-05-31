246cf2c25213e291429d1703c5180cf0cc6fe1be
Andrew Dunn <atd2005@gmail.com>
2024-05-30 21:53:01 -0500

    add github connection

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -13,6 +13,6 @@ new MyPipelineStack(app, "MyPipelineStack", {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: "637423577773", region: "us-east-1" },
+  env: { account: "730335377532", region: "us-east-1" },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -21,9 +21,27 @@ export class MyPipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "MyPipeline",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("andrewtdunn/my-pipeline", "main"),
+        //input: CodePipelineSource.gitHub("andrewtdunn/my-pipeline", "main"),
+        input: CodePipelineSource.connection(
+          "andrewtdunn/my-pipeline",
+          "main",
+          {
+            connectionArn:
+              "arn:aws:codestar-connections:us-east-1:637423577773:connection/78b54ada-1f46-4e0d-8b5c-572f1c8ee882",
+          }
+        ),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
+
+    // // Connect Application stage (which contains the Stacks)
+    // const applicationStage = new ApplicationStage(this, "DeployStage", {
+    //   env: {
+    //     account: "730335377532",
+    //     region: "us-east-1",
+    //   },
+    //   version: "1",
+    // });
+    // pipeline.addStage(applicationStage);
   }
 }
