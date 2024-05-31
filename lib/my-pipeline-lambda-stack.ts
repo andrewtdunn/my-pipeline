532e26f194b6ee41e743ee428daba1a70d5758e5
Andrew Dunn <atd2005@gmail.com>
2024-05-30 23:52:09 -0500

    update lambda

--- lib/my-pipeline-lambda-stack.ts
+++ lib/my-pipeline-lambda-stack.ts
@@ -9,7 +9,14 @@ export class MyLambdaStack extends cdk.Stack {
     new Function(this, "LambdaFunction", {
       runtime: Runtime.NODEJS_18_X,
       handler: "index.handler",
-      code: new InlineCode('exports.handler = _ => "Hello, CDK!!";'),
+      code: new InlineCode(`exports.handler = async function(event) {
+        console.log("request:", JSON.stringify(event, undefined, 2));
+        return {
+          statusCode: 200,
+          headers: { "Content-Type": "text/plain" },
+          body: 'Hello, CDK!'
+        };
+      };`),
     });
   }
 }
