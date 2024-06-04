4c92daf9ebe6ca8e8f9d2f82c744e7ea78e0a157
Andrew Dunn <atd2005@gmail.com>
2024-06-04 14:16:41 -0500

    add react build to codebuild

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -32,7 +32,15 @@ export class MyPipelineStack extends cdk.Stack {
               "arn:aws:codestar-connections:us-east-1:637423577773:connection/78b54ada-1f46-4e0d-8b5c-572f1c8ee882",
           }
         ),
-        commands: ["npm ci", "npm run build", "npx cdk synth"],
+        commands: [
+          "cd react_code",
+          "npm i",
+          "npm run build",
+          "cd ..",
+          "npm ci",
+          "npm run build",
+          "npx cdk synth",
+        ],
       }),
     });
 
